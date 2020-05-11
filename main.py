f6e27f8a194f6b29ff4a0b5a12ac9fe8ef390d5d
Shrimadhav U K <shrimadhavuk@gmail.com>
2020-05-11 18:26:28 +0530

    remove bot_client

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import datetime
 import pyrogram
 
 user_session_string = os.environ.get("user_session_string")
-bot_token = os.environ.get("bot_token")
 bots = [i.strip() for i in os.environ.get("bots").split(' ')]
 bot_owner = os.environ.get("bot_owner")
 update_channel = os.environ.get("update_channel")
@@ -15,43 +14,40 @@ api_hash = os.environ.get("api_hash")
 
 user_client = pyrogram.Client(
     user_session_string, api_id=api_id, api_hash=api_hash)
-bot_client = pyrogram.Client(
-    "bot", bot_token=bot_token, api_id=api_id, api_hash=api_hash)
 
 
 def main():
     with user_client:
-        with bot_client:
-            while True:
-                print("[INFO] starting to check uptime..")
-                edit_text = f"@{update_channel} Bot's Uptime Status.(Updated every 15 mins)\n\n"
-                for bot in bots:
-                    print(f"[INFO] checking @{bot}")
-                    snt = user_client.send_message(bot, '/start')
+        while True:
+            print("[INFO] starting to check uptime..")
+            edit_text = f"@{update_channel} Bot's Uptime Status.(Updated every 15 mins)\n\n"
+            for bot in bots:
+                print(f"[INFO] checking @{bot}")
+                snt = user_client.send_message(bot, '/start')
 
-                    time.sleep(15)
+                time.sleep(15)
 
-                    msg = user_client.get_history(bot, 1)[0]
-                    if snt.message_id == msg.message_id:
-                        print(f"[WARNING] @{bot} is down")
-                        edit_text += f"@{bot} status: `Down`\n\n"
-                        user_client.send_message(bot_owner,
-                                                 f"@{bot} status: `Down`")
-                    else:
-                        print(f"[INFO] all good with @{bot}")
-                        edit_text += f"@{bot} status: `Up`\n\n"
-                    user_client.read_history(bot)
+                msg = user_client.get_history(bot, 1)[0]
+                if snt.message_id == msg.message_id:
+                    print(f"[WARNING] @{bot} is down")
+                    edit_text += f"@{bot} status: `Down`\n\n"
+                    user_client.send_message(bot_owner,
+                                             f"@{bot} status: `Down`")
+                else:
+                    print(f"[INFO] all good with @{bot}")
+                    edit_text += f"@{bot} status: `Up`\n\n"
+                user_client.read_history(bot)
 
-                utc_now = datetime.datetime.utcnow()
-                ist_now = utc_now + datetime.timedelta(minutes=30, hours=5)
+            utc_now = datetime.datetime.utcnow()
+            ist_now = utc_now + datetime.timedelta(minutes=30, hours=5)
 
-                edit_text += f"__last checked on \n{str(utc_now)} UTC\n{ist_now} IST__"
+            edit_text += f"__last checked on \n{str(utc_now)} UTC\n{ist_now} IST__"
 
-                bot_client.edit_message_text(update_channel, status_message_id,
-                                             edit_text)
-                print(f"[INFO] everything done! sleeping for 15 mins...")
-                
-                time.sleep(15 * 60)
+            user_client.edit_message_text(update_channel, status_message_id,
+                                         edit_text)
+            print(f"[INFO] everything done! sleeping for 15 mins...")
+
+            time.sleep(15 * 60)
 
 
 main()

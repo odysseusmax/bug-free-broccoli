a680b3340e2cf6342ac35d43c467b1e491708af2
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-14 16:04:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import os
 import time
 import datetime
-
+import pytz
 import pyrogram
 
-user_session_string = os.environ.get("user_session_string")
-bots = [i.strip() for i in os.environ.get("bots").split(' ')]
-bot_owner = os.environ.get("bot_owner")
-update_channel = os.environ.get("update_channel")
-status_message_id = int(os.environ.get("status_message_id"))
-api_id = int(os.environ.get("api_id"))
-api_hash = os.environ.get("api_hash")
+# your session strings
+user_session_string = os.environ.get("SESSION_STRING")
+
+# your all bots username without '@' with a space 1 to another
+bots = [i.strip() for i in os.environ.get("BOTS").split(' ')]
+
+# your username without '@'
+bot_owner = os.environ.get("BOT_OWNER")
+
+# your channel username without '@'
+update_channel = os.environ.get("UPDATE_CHANNEL")
+
+# message id of your channel bot status message
+status_message_id = int(os.environ.get("STATUS_MESSAGE_ID"))
+
+# api strings from my.telegram.org
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
 
 user_client = pyrogram.Client(
     user_session_string, api_id=api_id, api_hash=api_hash)
@@ -30,21 +41,20 @@ def main():
                 msg = user_client.get_history(bot, 1)[0]
                 if snt.message_id == msg.message_id:
                     print(f"[WARNING] @{bot} is down")
-                    edit_text += f"@{bot} status: `Down`\n\n"
+                    edit_text += f"- <a href='https://telegram.me/{bot}'>{bot}</a> : <code>Offline</code>\n"
                     user_client.send_message(bot_owner,
                                              f"@{bot} status: `Down`")
                 else:
                     print(f"[INFO] all good with @{bot}")
-                    edit_text += f"@{bot} status: `Up`\n\n"
+                    edit_text += f"- <a href='https://telegram.me/{bot}'>{bot}</a> : <code>Online</code>\n"
                 user_client.read_history(bot)
 
-            utc_now = datetime.datetime.utcnow()
-            ist_now = utc_now + datetime.timedelta(minutes=30, hours=5)
+            utc_now = datetime.datetime.now(pytz.timezone('UTC')).strftime("%d/%m/%y %I:%M %p")
+            ist_now = datetime.datetime.now(pytz.timezone('IST')).strftime("%d/%m/%y %I:%M %p")
 
-            edit_text += f"__last checked on \n{str(utc_now)} UTC\n{ist_now} IST__"
+            edit_text += f"\n<b>Last checked:</b>\n{str(utc_now)} UTC\n{str(ist_now)} IST"
 
-            user_client.edit_message_text(update_channel, status_message_id,
-                                         edit_text)
+            user_client.edit_message_text(update_channel, status_message_id, text=edit_text, disable_web_page_preview=True, parse_mode="html")
             print(f"[INFO] everything done! sleeping for 15 mins...")
 
             time.sleep(15 * 60)
